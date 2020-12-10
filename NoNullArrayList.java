71e2edf7e0c6c617e5cd5a953cf46ee23cb23ff2
Yaying <yliangli20@stuy.edu>
2020-12-09 23:41:32 -0500

    added add(E e) method

--- NoNullArrayList.java
+++ NoNullArrayList.java
@@ -12,5 +12,14 @@ public class NoNullArrayList<T> extends ArrayList<T> {
     super(startingCapacity);
   }
 
+  //overriding add methods, so that they throw IllegalArgumentException
+  //when null is added
+  public boolean add(T element) { //dunno element's type rn, so use generic type T
+    if (element == null) {
+      throw new IllegalArgumentException("thou shall not add null into this ArrayList");
+    }
+    return super.add(element); //calling ArrayList's .add(element) method
+  }
+
 
 }
--- Tester.java
+++ Tester.java
@@ -7,9 +7,31 @@ public class Tester {
     //methods like .size() and the like, NoNullArrayList has those methods
     //because it got inheritted from ArrayList :D
     System.out.println(snake.size());
+    System.out.println(snake.add("ss"));
+    System.out.println(snake.add("sss"));
+    for (String element: snake) {
+      System.out.print("" + snake.get(snake.indexOf(element)) + " ");
+    }
+    System.out.println();
+    try {
+      snake.add(null);
+    }
+    catch (IllegalArgumentException e) {
+      System.out.println("ay gratz you got the IllegalArgumentException");
+    }
 
+
+    System.out.println();
     NoNullArrayList<Integer> ape = new NoNullArrayList<>(50);
     //ape got initialCapacity of 50
     System.out.println(ape.size());
+    for (int i = 0; i < 10; i++) {
+      System.out.print(ape.add(i));
+    }
+    System.out.println();
+    for (Integer element: ape) {
+      System.out.print("" + ape.get(element) + " ");
+    }
+    System.out.println();
   }
 }
